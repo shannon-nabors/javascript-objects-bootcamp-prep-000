0fd7a1cf24fe13d3dab670075eef6c8469061e4f
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-05 20:50:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
- function updateObjectWithKeyAndValue (obj, key, value) {
+ function updateObjectWithKeyAndValue(obj, key, value) {
    return Object.assign({}, obj, { [key]: value });
  }
  
  const recipe = { eggs: 3 };
+ 
+ updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup');
