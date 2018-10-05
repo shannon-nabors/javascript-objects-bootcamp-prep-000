07a94271807a81ee0b65759de5eab123298bac62
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-05 20:56:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,12 @@
  
  console.log(recipe);
  
+ var targetObject = {
+   '16th': 'Nicaragua',
+   '17th': 'Cuba',
+   '18th': 'Groucho Marx',
+   '19th': 'Mindanao'
+ }
  
  function updateObjectWithObject(targetObject, updatesObject) {
    console.log(Object.assign({}, targetObject, updatesObject));
